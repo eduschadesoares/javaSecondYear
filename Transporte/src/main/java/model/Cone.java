c8b02d7cd79709628467bcd31a45990a906bf9e7
Eduardo Soares <eduschadesoares@users.noreply.github.com>
2018-06-23 00:16:05 -0300

    Update Cone.java

--- Transporte/src/main/java/model/Cone.java
+++ Transporte/src/main/java/model/Cone.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author Idomar
- */
 public class Cone extends FiguraGeo3d {
 
     double diametro, raio;
